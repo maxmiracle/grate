36f7da6ecaeef15e758076da9f9392bd48cd284a
Maxim Vasiliev <vasiliev.maxim@gmail.com>
2018-09-03 20:31:05 +0300

    Create NetModel4 sln with Net.csproj

---
+++ colornet/NetModel4/Net/Test/ModelParametersTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.Test
+{
+    
+    class ModelParametersTest
+    {
+        [Test]
+        public void MaxObjectsTest()
+        {
+            ModelParameters modelParameters = new ModelParameters();
+            Assert.That(modelParameters.MaxObjects, Is.GreaterThan(0), "ModelParametes.MaxObjects should be greater than 0");
+            Assert.That(modelParameters.MinObjects, Is.GreaterThan(0), "ModelParametes.MinObjects should be greater than 0");
+            Assert.That(modelParameters.MaxObjects, Is.GreaterThan(modelParameters.MinObjects), "ModelParameters.MaxObjects should be greater than ModelParameters.MinObjects");
+        }
+    }
+}
